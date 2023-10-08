33153165b1443f33878180c8c61593d7435d794e
MariiaNebesnova <82033805+MariiaNebesnova@users.noreply.github.com>
2023-10-08 12:26:28 +0300

    update API

--- src/constants/apiPaths.ts
+++ src/constants/apiPaths.ts
@@ -1,8 +1,8 @@
 const API_PATHS = {
-  product: "https://.execute-api.eu-west-1.amazonaws.com/dev",
+  product: `https://${import.meta.env.VITE_API_PRODUCTS}.execute-api.us-east-1.amazonaws.com/dev`,
   order: "https://.execute-api.eu-west-1.amazonaws.com/dev",
   import: "https://.execute-api.eu-west-1.amazonaws.com/dev",
-  bff: "https://.execute-api.eu-west-1.amazonaws.com/dev",
+  bff: `https://${import.meta.env.VITE_API_PRODUCTS}.execute-api.us-east-1.amazonaws.com/dev`,
   cart: "https://.execute-api.eu-west-1.amazonaws.com/dev",
 };
 

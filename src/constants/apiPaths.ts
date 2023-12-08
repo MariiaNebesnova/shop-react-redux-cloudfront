47aed4145bf7a9b1e8e58c02c9a66d751ac4ac70
MariiaNebesnova <82033805+MariiaNebesnova@users.noreply.github.com>
2023-12-08 11:42:32 +0300

    task-8

--- src/constants/apiPaths.ts
+++ src/constants/apiPaths.ts
@@ -3,7 +3,7 @@ const API_PATHS = {
   order: "https://.execute-api.us-east-1.amazonaws.com/dev",
   import: `https://${import.meta.env.VITE_API_IMPORT_SERVICE}.execute-api.us-east-1.amazonaws.com/dev`,
   bff: `https://${import.meta.env.VITE_API_PRODUCTS}.execute-api.us-east-1.amazonaws.com/dev`,
-  cart: "https://.execute-api.us-east-1.amazonaws.com/dev",
+  cart: `https://${import.meta.env.VITE_API_CART}.execute-api.us-east-1.amazonaws.com/dev`,
 };
 
 export default API_PATHS;
